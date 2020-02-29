7ee7d7223a1455401da84f7a4f9aed33eb61cdff
iyaja <ajayuppili@gmail.com>
2020-02-29 05:44:11 -0600

    feat: add continuous integration

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="eve", # Replace with your own username
+    version="0.0.1",
+    author="Ajay Uppili Arasanipalai",
+    author_email="ajayuppili@gmail.com",
+    description="The EVer Evolving Optimizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iyaja/eve",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
